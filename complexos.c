fcb34414d35290d70a58778db4259b42e0edb721
emerson-santos <emerson@poli.ufrj.br>
2017-05-29 14:39:03 -0300

    Update complexos.c

--- complexos.c
+++ complexos.c
@@ -19,6 +19,6 @@ void dividir(double *a,double *b, double *c)
   d=c[0]*c[0]+c[1]c[1];
   a[0]=b[0]*c[0]+b[1]*c[1];
   a[0]=a[0]/d;
-  a[1]=b[0]*c[1]-b[1]*c[0];
+  a[1]=b[1]*c[0]-b[0]*c[1];
   a[1]=a[1]/d;
 }
