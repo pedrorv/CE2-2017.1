c39df403e14dfbe69a7f6c4568e193bb125b32fa
emerson-santos <emerson@poli.ufrj.br>
2017-06-01 13:56:26 -0300

    Update tipos.h

--- tipos.h
+++ tipos.h
@@ -1,3 +1,9 @@
+#define MAX_LINHA 80
+#define MAX_NOME 11
+#define MAX_ELEM 50
+#define MAX_NOS 50
+#define TOLG 1e-9
+#define GMAX 1000000
 
 typedef struct elemento { /* Elemento do netlist */
   char nome[MAX_NOME],la,lb;
