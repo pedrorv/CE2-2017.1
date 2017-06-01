9e2fae801fc39b9da34425a6606ee716f2f4f33a
emerson-santos <emerson@poli.ufrj.br>
2017-06-01 14:06:41 -0300

    Update mna.h

--- mna.h
+++ mna.h
@@ -9,3 +9,5 @@ void transcondutancia(double gm,int n1,int n2,int n3,int n4, Yn[MAX_NOS+1][MAX_N
 void condutancia(double g, int a, int b, Yn[MAX_NOS+1][MAX_NOS+2], tabela L, tabela C);
 
 void corrente(double i, int a, int b, Yn[MAX_NOS+1][MAX_NOS+2], tabela L, tabela C);
+
+void mnaPO(elemento netlist[MAX_ELEM],Yn[MAX_NOS+1][MAX_NOS+2]);
