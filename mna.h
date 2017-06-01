6ce587f15763c39dd433bec7ba9ad642ba3514f6
emerson-santos <emerson@poli.ufrj.br>
2017-06-01 13:55:41 -0300

    Create mna.h

---
+++ mna.h
@@ -0,0 +1,11 @@
+#include "tipos.h"
+
+void somar(int *Q, int a, int b);
+
+void operacional (int na,int nb,int nc,int nd, tabela L, tabela C);
+
+void transcondutancia(double gm,int n1,int n2,int n3,int n4, Yn[MAX_NOS+1][MAX_NOS+2], tabela L, tabela C);
+
+void condutancia(double g, int a, int b, Yn[MAX_NOS+1][MAX_NOS+2], tabela L, tabela C);
+
+void corrente(double i, int a, int b, Yn[MAX_NOS+1][MAX_NOS+2], tabela L, tabela C);
