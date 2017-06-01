755268498441f93dfc0eb07c987889648119854f
emerson-santos <emerson@poli.ufrj.br>
2017-06-01 14:06:45 -0300

    Update mna.c

--- mna.c
+++ mna.c
@@ -37,3 +37,35 @@ void corrente(double i, int a, int b, Yn[MAX_NOS+1][MAX_NOS+2], tabela L, tabela
   Yn[L[a]][neq+1]-=i;
   Yn[L[b]][neq+1]+=i;
 }
+
+void mnaPO(elemento netlist[MAX_ELEM],Yn[MAX_NOS+1][MAX_NOS+2]){
+  char tipo;
+  int i;
+  for (i=1; i<=ne; i++) {
+    tipo=netlist[i].nome[0];
+    if (tipo=='R') condutancia(1/netlist[i].valor,netlist[i].a,netlist[i].b);
+    else if (tipo=='L') condutancia(GMAX,netlist[i].a,netlist[i].b);
+    else if (tipo=='C') condutancia(0,netlist[i].a,netlist[i].b);
+    else if (tipo=='G') transcondutancia(netlist[i].valor,netlist[i].a,netlist[i].b,netlist[i].c,netlist[i].d);
+    else if (tipo=='K');
+    else if (tipo=='I') corrente(netlist[i].valor,netlist[i].a,netlist[i].b);
+    else if (tipo=='V') {
+      transcondutancia(1,0,netlist[i].x,netlist[i].a,netlist[i].b);
+      corrente(netlist[i].valor,netlist[i].x,0);
+    }
+    else if (tipo=='E') {
+      transcondutancia(1,0,netlist[i].x,netlist[i].a,netlist[i].b);
+      transcondutancia(netlist[i].valor,netlist[i].x,0,netlist[i].c,netlist[i].d);
+    }
+    else if (tipo=='F') {
+      transcondutancia(netlist[i].valor,netlist[i].a,netlist[i].b,netlist[i].x,0);
+      transcondutancia(1,netlist[i].c,netlist[i].d,netlist[i].x,0);
+    }
+    else if (tipo=='H') {
+      transcondutancia(1,0,netlist[i].y,netlist[i].a,netlist[i].b);
+      transcondutancia(netlist[i].valor,netlist[i].y,0,netlist[i].x,0);
+      transcondutancia(1,netlist[i].c,netlist[i].d,netlist[i].x,0);
+    }
+    else if (tipo=='O') {
+    }
+}
