431ba69525958e6bed091963356c468a64598437
emerson-santos <emerson@poli.ufrj.br>
2017-06-01 15:02:10 -0300

    Update mna.h

--- mna.h
+++ mna.h
@@ -4,10 +4,16 @@ void somar(int *Q, int a, int b);
 
 void operacional (int na,int nb,int nc,int nd, tabela L, tabela C);
 
-void transcondutancia(double gm,int n1,int n2,int n3,int n4, Yn[MAX_NOS+1][MAX_NOS+2], tabela L, tabela C);
+void transcondutancia(double gm,int n1,int n2,int n3,int n4,double Yn[MAX_NOS+1][MAX_NOS+2], tabela L, tabela C);
 
-void condutancia(double g, int a, int b, Yn[MAX_NOS+1][MAX_NOS+2], tabela L, tabela C);
+void condutancia(double g, int a, int b,double Yn[MAX_NOS+1][MAX_NOS+2], tabela L, tabela C);
 
-void corrente(double i, int a, int b, Yn[MAX_NOS+1][MAX_NOS+2], tabela L, tabela C);
+void admitancia(double _Complex  y, int a, int b, double _Complex Yn[MAX_NOS+1][MAX_NOS+2], tabela L, tabela C);
 
-void mnaPO(elemento netlist[MAX_ELEM],Yn[MAX_NOS+1][MAX_NOS+2]);
+void corrente(double i, int a, int b,double  Yn[MAX_NOS+1][MAX_NOS+2], tabela L, tabela C);
+
+void fasorcorrente(double modulo, double fase, int a, int b, double _Complex Yn[MAX_NOS+1][MAX_NOS+2], tabela L, tabela C);
+
+void mnaPO(elemento netlist[MAX_ELEM], double Yn[MAX_NOS+1][MAX_NOS+2], double Yn1[MAX_NOS+1][MAX_NOS+2], tabela L, tabela C);
+
+void mnaPS(elemento netlist[MAX_ELEM], double _Complex Yn[MAX_NOS+1][MAX_NOS+2], tabela L, tabela C, double f);
