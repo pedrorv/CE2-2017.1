e83f7cf024ec0fb731433b5b6773e9997b370062
emerson-santos <emerson@poli.ufrj.br>
2017-06-02 14:42:20 -0300

    Create resolver.h

---
+++ resolver.h
@@ -0,0 +1,8 @@
+#include "tipos.h"
+
+
+void resolversistemaPO(double  Yn[MAX_NOS+1][MAX_NOS+2],int neq);
+
+int testeconvergenciaPO(double  Yn[MAX_NOS+1][MAX_NOS+2], double  Yn1[MAX_NOS+1][MAX_NOS+2], int neq);
+
+void resolversistemaPS(double _Complex Yn[MAX_NOS+1][MAX_NOS+2], int neq);
